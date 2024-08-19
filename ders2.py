e833b6a88e139302671ac12e14221e0fa244164c
Kenan McCall <116302083+KenanMcCall@users.noreply.github.com>
2024-08-19 21:03:26 +0400

    Update ders2.py

--- ders2.py
+++ ders2.py
@@ -1,10 +1,16 @@
 import random
 
 karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-uzunluk = int(input("Uzunluk seçiniz: "))
+uzunluk = int(input("Şifrenizin uzunluğunu seçiniz: "))
+uzunluk2 = int(input("İkinci şifrenizin uzunluğunu seçiniz: "))
 sifre = ""
+sifre2 = ""
 
 for i in range(uzunluk):
     sifre += random.choice(karakterler)
+    sifre2 += random.choice(karakterler)
 
-print(sifre)
+for i in range(uzunluk2):
+    sifre2 += random.choice(karakterler)
+
+print("Şifre:", sifre, "Şifre 2:", sifre2)
