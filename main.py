50a0138920b122c859a2a9d95147bd80fc7cdf0d
Kenan McCall <116302083+KenanMcCall@users.noreply.github.com>
2024-09-16 21:20:46 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands, tasks
+import os, random
+from itertools import cycle
+import requests
 from bot_console import *
 from bot_console import gen_pass
 
@@ -10,9 +13,9 @@ intents.messages = True
 bot = commands.Bot(command_prefix='.', intents=intents)
 
 activities = [
-    discord.Game(name="By Kenan McCall"),
-    discord.Activity(type=discord.ActivityType.listening, name="Kenan McCall'ın Hizmetinde!"),
-    discord.Activity(type=discord.ActivityType.watching, name="Sunucu Güvenliğini Sağlıyor!")
+    discord.Game(name = "By Kenan McCall"),
+    discord.Activity(type=discord.ActivityType.listening, name = "Kenan McCall'ın Hizmetinde!"),
+    discord.Activity(type=discord.ActivityType.watching, name = "Sunucu Güvenliğini Sağlıyor!")
 ]
 
 statuses = [
@@ -21,7 +24,7 @@ statuses = [
     discord.Status.dnd,
 ]
 
-@tasks.loop(minutes = 3)  
+@tasks.loop(minutes = 5)  
 async def update_status():
     activity = random.choice(activities)
     status = random.choice(statuses)
@@ -41,13 +44,36 @@ async def on_message(message):
         await message.channel.send("Aleyküm Selam!")
     await bot.process_commands(message)
 
+
+@bot.command()
+async def mem(ctx):
+    resim_adi = random.choice(os.listdir("images"))
+    file_path = f'images/{resim_adi}' 
+    try:
+        with open(file_path, 'rb') as f:
+            picture = discord.File(f)  
+        await ctx.send(file=picture) 
+    except FileNotFoundError:
+        await ctx.send("Fotoğraf bulunamadı.")  
+
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
 @bot.command()
 async def sifre(ctx):
     await ctx.send(gen_pass(10))
-
-@bot.command()
-async def emoji(ctx):
-    await ctx.send()
 
 @bot.command()
 async def yazitura(ctx):
@@ -77,4 +103,44 @@ async def sil(ctx, limit: int):
     deleted = await ctx.channel.purge(limit=limit)
     await ctx.send(f"**{len(deleted)}** *mesaj silindi.*", delete_after=2)
 
-bot.run("token")
+duyuru_mesajlari = cycle([
+    "**🌍 Çevremizi Temiz Tutalım!**\n\n"
+    "*Temiz bir çevre sağlıklı bir yaşam için en temel gereksinimlerden biridir.* "
+    "*Çevremizi temiz tutmak için lütfen aşağıdaki adımlara dikkat edelim* :\n\n"
+    "**1.** ♻️ Atıklarınızı doğru şekilde geri dönüştürün.\n"
+    "**2.** 🗑️ Çöplerinizi uygun alanlara atın.\n"
+    "**3.** 💡 Enerji tasarrufu yapın ve doğal kaynakları koruyun.\n"
+    "**4.** 🚮 Sokakları ve parkları temiz tutalım, çöplerimizi etrafa bırakmayalım.\n\n"
+    "*Hep birlikte daha yaşanabilir bir dünya için adım atalım!* \n<@&1219368556031115354>",
+
+    "**♻️ Geri Dönüşümün Önemi!**\n\n"
+    "*Geri dönüşüm, doğal kaynakları korumanın ve enerji tasarrufu yapmanın harika bir yoludur.* "
+    "*Her atık doğru bir şekilde geri dönüştürülmelidir* :\n\n"
+    "**1.** Cam, kağıt ve plastik atıklarınızı ayrı ayrı biriktirin.\n"
+    "**2.** Geri dönüşüm kutularını kullanın ve atıkları ayırın.\n"
+    "**3.** Elektronik atıkları özel toplama merkezlerine teslim edin.\n\n"
+    "*Doğayı korumak için geri dönüşüm bilincini yayalım!* \n<@&1219368556031115354>",
+
+    "**🌱 Doğal Kaynakları Korumak**\n\n"
+    "*Doğal kaynaklar sınırlıdır ve onları doğru kullanmak zorundayız.* "
+    "*Bunun için şu adımları takip edelim* :\n\n"
+    "**1.** 💧 Su tasarrufu yapın, muslukları gereksiz yere açık bırakmayın.\n"
+    "**2.** 🔋 Yenilenebilir enerji kaynaklarını tercih edin.\n"
+    "**3.** 🍃 Doğal ürünler tüketmeye özen gösterin.\n\n"
+    "*Daha yeşil bir gelecek için bugünden harekete geçelim!* \n<@&1219368556031115354>"
+])
+
+@tasks.loop(seconds=10)
+async def cevre_bilgilendirme_gorevi(duyuru_kanal_id):
+    duyuru_kanal = bot.get_channel(duyuru_kanal_id)
+    duyuru_mesaji = next(duyuru_mesajlari)
+
+    if duyuru_kanal:
+        await duyuru_kanal.send(duyuru_mesaji)
+
+@bot.command()
+async def duyuru(ctx):
+    duyuru_kanal_id = 1219368610166997053
+    cevre_bilgilendirme_gorevi.start(duyuru_kanal_id)
+
+bot.run("TOKEN")
